51f035f4d69e924750abab0f5aefe8636c54afb6
Emil Pedersen <emil.pedersen@epitech.eu>
2023-04-25 14:31:51 +0200

    feat: create enum material

--- src/shape.rs
+++ src/shape.rs
@@ -8,6 +8,18 @@
 mod intersection;
 use intersection::{Intersection, Ray};
 
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+enum Material {
+    Color(Color),
+    Mirror,
+}
+
 /// Shape trait
 ///
 /// This trait is used to define a shape.
@@ -17,8 +29,6 @@ pub trait Shape {
     /// This function returns an Intersection if the ray intersects the shape.
     /// If the ray doesn't intersect the shape, it returns None.
     fn intersect(&self, ray: &Ray) -> Option<Intersection>;
-    /// Get the color of a shape
-    fn getColor(&self) -> Color;
     /// Get the material of a shape
     fn getMaterial(&self) -> Material;
 }
