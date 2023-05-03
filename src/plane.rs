d84f15a30d9c48ac71a371269e77636e09ae811f
Yunse LEE <yunse1.lee@epitech.eu>
2023-05-03 16:04:16 +0200

    style: add end of line and comments

--- src/plane.rs
+++ src/plane.rs
@@ -41,20 +41,3 @@ impl Shape for Plane {
         )
     }
 }
-
-// struct plane *p = obj;
-// double d = -vector_product(&p->pos, &p->normal);
-// double divi = vector_product(&r->direction, &p->normal);
-// double t = -(vector_product(&p->normal, &r->origin) + d) / divi;
-
-// if (t <= 0 || divi == 0)
-//     return false;
-// out->intersection = (struct vector) {r->origin.x + t * r->direction.x,
-//     r->origin.y + t * r->direction.y, r->origin.z + t * r->direction.z};
-// out->distance = vector_distance(&r->origin, &out->intersection);
-// if (divi > 0)
-//     out->normal = (struct vector) {-p->normal.x,
-//         -p->normal.y, -p->normal.z};
-// else
-//     out->normal = p->normal;
-// return true;
