f3dcb7e69461778ec7b5d6fc5732cc9746d77c41
jerem <jeremy.elalouf@epitech.eu>
2023-05-03 16:05:57 +0200

    feat: branch initial commit

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,6 @@ mod shape;
 mod material;
 mod camera;
 
-use vector3d::Vector3D;
 
 fn main() {
-    let mut vector3d = Vector3D {x: 3.0, y: 4.0, z: 5.0};
-    let second_vector3d = Vector3D {x: 1.0, y: 1.0, z: 1.0};
-    vector3d += second_vector3d;
-    vector3d -= second_vector3d;
-
-    println!("{:?}", vector3d);
-    println!("{:?}", vector3d.length());
 }
