fee4311a9b83ef604e71e17a95637574e36a1ac6
jerem <jeremy.elalouf@epitech.eu>
2023-05-04 14:44:48 +0200

    feat: add get and set methods to the framebuffer

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -7,6 +7,17 @@ use std::vec::Vec;
 use crate::material::Color;
 
 pub struct Framebuffer {
-    pub colors: Vec<Color>
+    pub colors: Vec<Color>,
+    pub height: usize,
+    pub width: usize
+
 }
 
+impl Framebuffer {
+    pub fn get(&self, x: usize, y: usize)-> &Color {
+        &self.colors[(self.width * y) + x]
+    }
+    pub fn set(&mut self, x: usize, y: usize, val: Color) {
+        self.colors[(self.width * y) + x] = val
+    }
+}
