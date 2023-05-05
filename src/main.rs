a57e44c611f3167546964d2320d03a97a5e21c50
Lindon ALIU <lindon.aliu@epitech.eu>
2023-05-05 18:08:09 +0200

    refactor: change framebuffer to imageRgb

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ mod vector3d;
 mod shape;
 mod material;
 mod camera;
-mod framebuffer;
 mod intersections {
     pub mod plane;
     pub mod sphere;
@@ -30,12 +29,14 @@ use crate::intersection::Ray;
 use material::Material;
 use material::Color;
 use scene::Scene;
-use framebuffer::Framebuffer;
 
 use nannou::prelude::*;
 use nannou::image::ImageBuffer;
 use nannou::wgpu;
 use nannou::wgpu::Device;
+use nannou::image::RgbImage;
+use nannou::image::DynamicImage; 
+use nannou::image::DynamicImage::ImageRgb8;
 
 fn main() {
     nannou::app(model).update(update).run();
@@ -43,50 +44,31 @@ fn main() {
 
 struct Model {
     window: window::Id,
-    framebuffer: Framebuffer,
-    texture: wgpu::Texture,
+    framebuffer: DynamicImage,
+    sizes: (u32, u32),
 }
 
 fn model(app: &App) -> Model {
-    let width = 1920;
-    let height = 1080;
-    let window = app.new_window().size(width, height).view(view).build().unwrap();
-    let framebuffer = Framebuffer::new(width as usize, height as usize);
-    let texture = wgpu::TextureBuilder::new()
-        .size([width, height])
-        .format(wgpu::TextureFormat::Rgba8Unorm)
-        .build(app.main_window().device());
-    Model { window, framebuffer, texture } 
+    let sizes = (1920, 1080);
+    let window = app.new_window().size(sizes.0, sizes.1).view(view).build().unwrap();
+    let framebuffer = ImageRgb8(ImageBuffer::new(sizes.0, sizes.1));
+
+    Model { sizes, window, framebuffer }
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
 }
 
-fn colors_to_u8_array(colors: &Vec<Color>) -> Vec<u8> {
-    let mut u8_colors = Vec::new();
-    for color in colors {
-        u8_colors.push(color.r);
-        u8_colors.push(color.g);
-        u8_colors.push(color.b);
-        u8_colors.push(color.a);
-    }
-    u8_colors
-}
-
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    let framebuffer = &model.framebuffer;
-    let texture = &model.texture;
-
-    texture.upload_data(
-        app.main_window().device(),
-        &mut *frame.command_encoder(),
-        &colors_to_u8_array(&framebuffer.colors),
+    let texture = wgpu::Texture::from_image(
+        app,
+        &model.framebuffer,
     );
 
-    draw.texture(texture)
+    draw.texture(&texture)
         .x_y(0.0, 0.0)
-        .w_h(framebuffer.width as f32, framebuffer.height as f32);
+        .w_h(model.sizes.0 as f32, model.sizes.1 as f32);
 
     draw.to_frame(app, &frame).unwrap();
 }
