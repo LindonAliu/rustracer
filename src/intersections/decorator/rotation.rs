f79f9c530f59c85ab87971413572b5ff2ba1c782
Lindon ALIU <lindon.aliu@epitech.eu>
2023-05-06 20:26:55 +0200

    feat: add Mul<Vector3D> impl for Transformation struct

--- src/intersections/decorator/rotation.rs
+++ src/intersections/decorator/rotation.rs
@@ -49,3 +49,22 @@ impl Mul for Transformation {
         Transformation(result)
     }
 }
+
+impl Mul<Vector3D> for Transformation {
+    type Output = Vector3D;
+
+    fn mul(self, other: Vector3D) -> Vector3D {
+        let mut result = [0.0; 4];
+        for i in 0..4 {
+            for j in 0..4 {
+                result[i] += self.0[i][j] * other[j];
+            }
+        }
+        Vector3D {
+            w: result[0],
+            x: result[1],
+            y: result[2],
+            z: result[3],
+        }
+    }
+}
