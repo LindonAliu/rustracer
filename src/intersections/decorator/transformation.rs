ad3f24cf277ee9cad196c337fdca14b6da38d822
Lindon ALIU <lindon.aliu@epitech.eu>
2023-05-06 23:58:54 +0200

    fix: update with matrix class

--- src/intersections/decorator/transformation.rs
+++ src/intersections/decorator/transformation.rs
@@ -5,57 +5,11 @@
 // FreeKOSOVO
 //
 
-use std::ops::{Add, AddAssign, Mul, MulAssign};
-
-use crate::intersection::{Intersection, Ray};
 use crate::vector3d::{Vector3D};
-use crate::shape::Shape;
+use crate::matrix::Matrix;
 
 pub struct Transformation {
-    pub matrix: [[f64; 4]; 4],
-    pub wrapped: Box<dyn Shape>,
-}
-
-impl Add for Transformation {
-    type Output = Transformation;
-
-    fn add(self, other: Transformation) -> Transformation {
-        let mut result = [[0.0; 4]; 4];
-        for i in 0..4 {
-            for j in 0..4 {
-                result[i][j] = self.matrix[i][j] + other.matrix[i][j];
-            }
-        }
-        Transformation {
-            matrix: result,
-            wrapped: self.wrapped,
-        }
-    }
-}
-
-impl AddAssign for Transformation {
-    fn add_assign(&mut self, other: Transformation) {
-        *self = *self + other;
-    }
-}
-
-impl Mul for Transformation {
-    type Output = Transformation;
-
-    fn mul(self, other: Transformation) -> Transformation {
-        let mut result = [[0.0; 4]; 4];
-        for i in 0..4 {
-            for j in 0..4 {
-                for k in 0..4 {
-                    result[i][j] += self.matrix[i][k] * other.matrix[k][j];
-                }
-            }
-        }
-        Transformation {
-            matrix: result,
-            wrapped: self.wrapped,
-        }
-    }
+    pub matrix: Matrix,
 }
 
 impl Mul<Vector3D> for Transformation {
@@ -74,16 +28,5 @@ impl Mul<Vector3D> for Transformation {
             y: result[2],
             z: result[3],
         }
-    }
-}
-
-impl MulAssign for Transformation {
-    fn mul_assign(&mut self, other: Transformation) {
-        *self = *self * other;
-    }
-}
-
-impl Shape for Transformation {
-    fn intersect(&self, ray: &Ray) -> Option<Intersection> {
     }
 }
