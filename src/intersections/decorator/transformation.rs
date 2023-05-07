daf8a6a12264091097552ffb313f229224483a58
Lindon ALIU <lindon.aliu@epitech.eu>
2023-05-08 00:37:12 +0200

    feat: add implementation for intersection of shape for transformation (doesnt work)

--- src/intersections/decorator/transformation.rs
+++ src/intersections/decorator/transformation.rs
@@ -5,6 +5,7 @@
 // FreeKOSOVO
 //
 
+use crate::intersection::{Intersection, Ray};
 use crate::matrix::Matrix;
 use crate::shape::Shape;
 use serde::{Deserialize};
@@ -29,6 +30,39 @@ impl From<SerTransformation> for Transformation {
             transformation: Matrix::from(other.transformation),
             reverse_transformation: Matrix::from(other.transformation).inverse(),
             wrapped: other.wrapped,
+        }
+    }
+}
+
+#[typetag::serde]
+impl Shape for Transformation {
+    fn intersect(&self, ray: &Ray) -> Option<Intersection> {
+        let new_origin = self.reverse_transformation * ray.origin;
+        let scaled_ray = Ray {
+            origin: new_origin,
+            direction: (self.reverse_transformation *
+                (ray.origin + ray.direction)) - new_origin,
+        };
+
+        if let Some(intersection) = self.wrapped.intersect(&scaled_ray) {
+            let mut new_intersection = self.transformation * intersection.intersection_point;
+            new_intersection.w = 1.0;
+            let mut distance = (ray.origin - new_intersection).length();
+            distance.w = 1.0;
+
+            let mut normal = self.transformation *
+                (intersection.intersection_point + intersection.normal) -
+                new_intersection;
+            normal.w = 1.0;
+
+            Some(Intersection {
+                intersection_point: new_intersection,
+                normal: normal,
+                distance: distance,
+                shape: self,
+            })
+        } else {
+            None
         }
     }
 }
