b458b968ff717af153a13a7baf83c1529271e989
Lindon ALIU <lindon.aliu@epitech.eu>
2023-05-07 13:05:37 +0200

    feat: add transposed and inverse method for Matrix and operator div with f64.

--- src/matrix.rs
+++ src/matrix.rs
@@ -5,7 +5,7 @@
 // FreeKOSOVO
 //
 
-use std::ops::{Add, AddAssign, Mul};
+use std::ops::{Add, AddAssign, Mul, Div};
 use crate::vector3d::Vector3D;
 
 pub struct Matrix {
@@ -97,6 +97,20 @@ impl Mul<Vector3D> for Matrix {
     }
 }
 
+impl Div<f64> for Matrix {
+    type Output = Matrix;
+
+    fn div(self, other: f64) -> Matrix {
+        let mut result = Matrix::new(self.rows, self.cols);
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                result[(i, j)] = self[(i, j)] / other;
+            }
+        }
+        result
+    }
+}
+
 impl Matrix {
     fn determinant(&self) -> f64 {
         let mut det = 0.0;
@@ -128,60 +142,41 @@ impl Matrix {
         }
         coeff * minor.determinant()
     }
+
+    fn transposed(&self) -> Matrix {
+        let mut transposed = Matrix::new(self.rows, self.cols);
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                transposed[(i, j)] = self[(j, i)];
+            }
+        }
+        transposed
+    }
+
+    fn inverse(&self) -> Matrix {
+        let mut inverse = Matrix::new(self.rows, self.cols);
+        let det = self.determinant();
+        
+        if det == 0.0 || self.rows != self.cols {
+            panic!("Matrix is not invertible");
+        }
+        match self.rows {
+            1 => inverse[(0, 0)] = 1.0 / self[(0, 0)],
+            2 => {
+                inverse[(0, 0)] = self[(1, 1)] / det;
+                inverse[(0, 1)] = -self[(0, 1)] / det;
+                inverse[(1, 0)] = -self[(1, 0)] / det;
+                inverse[(1, 1)] = self[(0, 0)] / det;
+            },
+            _ => {
+                for i in 0..self.rows {
+                    for j in 0..self.cols {
+                        inverse[(i, j)] = self.cofactor(i, j);
+                    }
+                }
+                inverse = inverse.transposed() / det;
+            }
+        }
+        inverse
+    }
 }
-
-//     fn determinant(&self) -> f64 {
-//         let mut det = 0.0;
-
-//         match self.rows {
-//             1 => det = self.matrix[0][0],
-//             2 => det = self.matrix[0][0] * self.matrix[1][1] - self.matrix[0][1] * self.matrix[1][0],
-//             _ => {
-//                 for i in 0..self.rows {
-//                     det += self.matrix[0][i] * self.cofactor(0, i);
-//                 }
-//             }
-//         }
-//         det
-//     }
-
-//     fn transposed(&self) -> Matrix {
-//         let mut transposed = Matrix::new(self.rows, self.cols);
-//         for i in 0..self.rows {
-//             for j in 0..self.cols {
-//                 transposed.matrix[i][j] = self.matrix[j][i];
-//             }
-//         }
-//         transposed
-//     }
-
-//     fn inverse(&self) -> Matrix {
-//         let mut inverse = Matrix::new(self.rows, self.cols);
-//         let det = self.determinant();
-
-//         match self.rows {
-//             1 => inverse.matrix[0][0] = 1.0 / self.matrix[0][0],
-//             2 => {
-//                 if det == 0.0 {
-//                     panic!("Matrix is not invertible");
-//                 }
-//                 inverse.matrix[0][0] = self.matrix[1][1] / det;
-//                 inverse.matrix[0][1] = -self.matrix[0][1] / det;
-//                 inverse.matrix[1][0] = -self.matrix[1][0] / det;
-//                 inverse.matrix[1][1] = self.matrix[0][0] / det;
-//             },
-//             _ => {
-//                 for rows in 0..self.rows {
-//                     for cols in 0..self.cols {
-//                         inverse.matrix[rows][cols] = self.cofactor(rows, cols);
-//                     }
-//                 }
-//                 if det == 0.0 {
-//                     panic!("Matrix is not invertible");
-//                 }
-//                 inverse = inverse.transposed() / det;
-//             }
-//         }
-//         inverse
-//     }
-// }
