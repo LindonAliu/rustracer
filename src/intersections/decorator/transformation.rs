462b679429752b6b9ce9f651873e23e8e549fc72
Lindon ALIU <lindon.aliu@epitech.eu>
2023-05-07 22:36:42 +0200

    feat: add Transformation decorator

--- src/intersections/decorator/transformation.rs
+++ src/intersections/decorator/transformation.rs
@@ -6,7 +6,29 @@
 //
 
 use crate::matrix::Matrix;
+use crate::shape::Shape;
+use serde::{Deserialize};
 
+#[derive(Deserialize)]
+pub struct SerTransformation {
+    pub transformation: [f64; 16],
+    pub wrapped: Box<dyn Shape>,
+}
+
+#[derive(Deserialize)]
+#[serde(from = "SerTransformation")]
 pub struct Transformation {
-    pub matrix: Matrix,
+    pub transformation: Matrix,
+    pub reverse_transformation: Matrix,
+    pub wrapped: Box<dyn Shape>,
+}
+
+impl From<SerTransformation> for Transformation {
+    fn from(other: SerTransformation) -> Self {
+        Transformation {
+            transformation: Matrix::from(other.transformation),
+            reverse_transformation: Matrix::from(other.transformation).inverse(),
+            wrapped: other.wrapped,
+        }
+    }
 }
