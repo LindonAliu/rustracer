a8081601b178b720bbb5259d4c1fb022f1c7c302
Lindon ALIU <lindon.aliu@epitech.eu>
2023-05-07 12:52:36 +0200

    fix: delete the implementation of transformation because is implementation of matrix and not transformation.

--- src/intersections/decorator/transformation.rs
+++ src/intersections/decorator/transformation.rs
@@ -5,30 +5,8 @@
 // FreeKOSOVO
 //
 
-use std::ops::Mul;
-
-use crate::vector3d::{Vector3D};
 use crate::matrix::Matrix;
 
 pub struct Transformation {
     pub matrix: Matrix,
-}
-
-impl Mul<Vector3D> for Transformation {
-    type Output = Vector3D;
-
-    fn mul(self, other: Vector3D) -> Vector3D {
-        let mut result = [0.0; 4];
-        for i in 0..4 {
-            for j in 0..4 {
-                result[i] += self.matrix[i][j] * other[j];
-            }
-        }
-        Vector3D {
-            w: result[0],
-            x: result[1],
-            y: result[2],
-            z: result[3],
-        }
-    }
 }
