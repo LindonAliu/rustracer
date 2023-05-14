e911221a1f903387254ce5c66f0df4a3176ecf43
Lindon ALIU <lindon.aliu@epitech.eu>
2023-05-14 17:46:56 +0200

    refactor(camera): clean the repetition of code

--- src/camera.rs
+++ src/camera.rs
@@ -19,41 +19,42 @@ pub struct Camera {
     pub fov: f64,
 }
 
-impl Camera {
+fn change_element(element: &mut f64, key: &Key, sign: &str, app: &App) {
+    let how_much = if app.keys.down.contains(&Key::LShift) || app.keys.down.contains(&Key::RShift) {
+        1.
+    } else {
+        0.1
+    };
+
+    if app.keys.down.contains(key) {
+        if sign == "+" {
+            *element += how_much;
+        } else {
+            *element -= how_much;
+        }
+    }
+}
+
+impl Camera {   
     fn update_position(&mut self, app: &App) {
-        if app.keys.down.contains(&Key::Left) {
-            self.position.x -= 0.1;
-        }
-        if app.keys.down.contains(&Key::Right) {
-            self.position.x += 0.1;
-        }
-        if app.keys.down.contains(&Key::Up) {
-            self.position.y -= 0.1;
-        }
-        if app.keys.down.contains(&Key::Down) {
-            self.position.y += 0.1;
-        }
-        if app.keys.down.contains(&Key::I) {
-            self.position.z -= 0.1;
-        }
-        if app.keys.down.contains(&Key::K) {
-            self.position.z += 0.1;
-        }
+        change_element(&mut self.position.x, &Key::Left, "-", app);
+        change_element(&mut self.position.x, &Key::Right, "+", app);
+
+        change_element(&mut self.position.y, &Key::Up, "+", app);
+        change_element(&mut self.position.y, &Key::Down, "-", app);
+
+        change_element(&mut self.position.z, &Key::I, "-", app);
+        change_element(&mut self.position.z, &Key::K, "+", app);
     }
 
     fn update_direction(&mut self, app: &App) {
-        if app.keys.down.contains(&Key::Z) || app.keys.down.contains(&Key::W) {
-            self.direction.y += 0.1;
-        }
-        if app.keys.down.contains(&Key::Q) || app.keys.down.contains(&Key::A) {
-            self.direction.x += 0.1;
-        }
-        if app.keys.down.contains(&Key::S) {
-            self.direction.y -= 0.1;
-        }
-        if app.keys.down.contains(&Key::D) {
-            self.direction.x -= 0.1;
-        }
+        change_element(&mut self.direction.y, &Key::Z, "+", app);
+        change_element(&mut self.direction.y, &Key::W, "+", app);
+        change_element(&mut self.direction.y, &Key::S, "-", app);
+
+        change_element(&mut self.direction.x, &Key::Q, "+", app);
+        change_element(&mut self.direction.x, &Key::A, "+", app);
+        change_element(&mut self.direction.x, &Key::D, "-", app);
     }
 
     pub fn update(&mut self, app: &App) {
