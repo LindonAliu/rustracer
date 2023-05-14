342173bc2ff300c45d0f13335c00d15d0fd9f990
Emil Pedersen <emil.pedersen@epitech.eu>
2023-05-14 23:17:49 +0200

    fix: camera movements around y

--- src/scene.rs
+++ src/scene.rs
@@ -21,7 +21,7 @@ pub struct Scene {
     pub lights: Vec<Box<dyn Light>>
 }
 
-fn get_matrix_direction_event(app: &App) -> Matrix {
+fn get_matrix_direction_event_y(app: &App) -> Matrix {
     let mut tb = TransformationBuilder::new();
     let how_much = if app.keys.down.contains(&Key::LShift) || app.keys.down.contains(&Key::RShift) {
         1.
@@ -30,11 +30,22 @@ fn get_matrix_direction_event(app: &App) -> Matrix {
     };
 
     if app.keys.down.contains(&Key::Q) || app.keys.down.contains(&Key::A) {
-        tb = tb.rotation_y(how_much);
-    }
-    if app.keys.down.contains(&Key::D) {
         tb = tb.rotation_y(-how_much);
     }
+    if app.keys.down.contains(&Key::D) {
+        tb = tb.rotation_y(how_much);
+    }
+    tb.get_matrix()
+}
+
+fn get_matrix_direction_event_x(app: &App) -> Matrix {
+    let mut tb = TransformationBuilder::new();
+    let how_much = if app.keys.down.contains(&Key::LShift) || app.keys.down.contains(&Key::RShift) {
+        1.
+    } else {
+        0.1
+    };
+
     if app.keys.down.contains(&Key::W) || app.keys.down.contains(&Key::Z) {
         tb = tb.rotation_x(how_much);
     }
@@ -77,9 +88,16 @@ impl Scene {
 
     pub fn update_direction(&mut self, app: &App) {
         let matrix_direction = get_camera_transformation(self);
-        let matrix_direction_event = get_matrix_direction_event(app);
+        let event_x = get_matrix_direction_event_x(app);
+        let event_y = get_matrix_direction_event_y(app);
 
-        self.camera.direction = matrix_direction.clone() * matrix_direction_event * matrix_direction.inverse() * self.camera.direction;
+        // self.camera.direction = matrix_direction.clone() * matrix_direction_event * matrix_direction.inverse() * self.camera.direction;
+        let direction = event_y * matrix_direction.clone() * event_x;
+
+        self.camera.direction.x = direction[(2, 0)];
+        self.camera.direction.y = direction[(2, 1)];
+        self.camera.direction.z = direction[(2, 2)];
+
     }
 
     pub fn update_position(&mut self, app: &App) {
